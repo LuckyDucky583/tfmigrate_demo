a1a997308b953a54b1dd13bd4e4d89d6a290a56f
Lucas Hornung <lucas.hornung@datadrivers.de>
2023-12-05 15:39:21 +0100

    fix: remove duplicate dynamodb table

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,16 +5,3 @@ terraform {
     region = "eu-central-1"
   }
 }
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  hash_key       = "LockID"
-  name           = "terraform-locks"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 1
-  write_capacity = 1
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
